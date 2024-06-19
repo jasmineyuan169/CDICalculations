77dfb35f883a0494c1cef849565e6690d77f34ea
jasmineyuan169 <jasmineyuan169@gmail.com>
2024-06-19 14:56:40 +0800

    2024-06-19 土地收储成本输入数据录入完毕

--- server.js
+++ server.js
@@ -15,10 +15,51 @@ app.use(bodyParser.json()); // 解析json、multi-part（文件）、url-encoded
 app.use('/public', express.static('public'));
 app.use('/pages', express.static('pages'));
 
-// API路由
+// 土地收储成本-输出值表
 app.get('/api/land-costs', (req, res) => {
   const category = req.query.category; // 获取前端传递的category参数
   const query = "SELECT serial_number as 序号, project_name as 项目名称, unit as 单位, unit_price as `单价（万元）`, cost as `成本（万元）` FROM land_acquisition_cost_estimate WHERE category = ?";
+  db.query(query, [category], (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      res.status(500).send('Error fetching data');
+      return;
+    }
+    res.status(200).json({
+      status: 0,
+      msg: '',
+      data: {
+        items: results
+      }
+    });
+  });
+});
+
+// 土地收储成本-输入表1（土地补偿指标）
+app.get('/api/land-cost-input', (req, res) => {
+  const category = req.query.category;
+  const computing_method = req.query.computing_method;
+  const query = "SELECT serial_number, indicator, value, remark, basis FROM land_acquisition_cost_input WHERE category = ? AND computing_method in (?,3)";
+  db.query(query, [category, computing_method], (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      res.status(500).send('Error fetching data');
+      return;
+    }
+    res.status(200).json({
+      status: 0,
+      msg: '',
+      data: {
+        items: results
+      }
+    });
+  });
+});
+
+app.get('/api/land-input', (req, res) => {
+  const category = req.query.category;
+  const computing_method = req.query.computing_method;
+  const query = "SELECT serial_number, indicator, value, remark, basis FROM land_acquisition_cost_input WHERE category = ?";
   db.query(query, [category], (err, results) => {
     if (err) {
       console.error('Error fetching data:', err);
