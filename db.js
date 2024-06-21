d3704d008bb7c4652abdbdeeb6f3d52f6668b88d
jasmineyuan169 <jasmineyuan169@gmail.com>
2024-06-21 14:25:22 +0800

    2024-06-21 1

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'user1',
+//   password: 'CDI2024',
+//   database: 'CDICalculations'
+// });
+
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user1',
-  password: 'CDI2024',
-  database: 'CDICalculations'
+  host: '100.100.100.5',
+  port: 6603,
+  user: 'CDIqys',
+  password: '6nI!xIGGC3kAdWQ6',
+  database: 'CDICalculations',
+  option: 3
 });
 
 connection.connect((err) => {
